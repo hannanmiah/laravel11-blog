c6a3fb8745a4f37cb3b6e60dcfa69278047b788d
Hannan Miah <hannanhridoy@gmail.com>
2024-04-01 19:53:18 +0600

    scaffold project with necessary package and settings

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('users', 'App\Http\Controllers\UserController');
+    Route::apiResource('posts', 'App\Http\Controllers\PostController');
+    Route::apiResource('comments', 'App\Http\Controllers\CommentController');
+    Route::apiResource('tags', 'App\Http\Controllers\TagController');
+    Route::apiResource('categories', 'App\Http\Controllers\CategoryController');
+});
